d82906b04427aeaa1968d4a49f58187582602da7
Birger Hildenbrandt <birger.hildenbrandt@quadriga.eu>
2026-05-27 06:29:37 +0200

    Kommentar ergänzt.

--- classes/mail.py
+++ classes/mail.py
@@ -1,4 +1,4 @@
 from classes.mail_model import Mail
 from classes.mail_repository import DictForMail
 
-__all__ = ["Mail", "DictForMail"]
+__all__ = ["Mail", "DictForMail"] # Reminder: Kontrolle des Exports
